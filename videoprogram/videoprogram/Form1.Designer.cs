2473490f8e6f12844f21a734e97640c6a8e3bd2f
GOSUwat <asus6972@gmail.com>
2021-04-20 16:46:30 +0300

    update

--- videoprogram/videoprogram/Form1.Designer.cs
+++ videoprogram/videoprogram/Form1.Designer.cs
@@ -31,37 +31,80 @@ namespace videoprogram
         {
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tick1 = new System.Windows.Forms.Label();
+            this.tick2 = new System.Windows.Forms.Label();
+            this.tick3 = new System.Windows.Forms.Label();
+            this.tick4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Himalaya", 25.25F);
-            this.label1.Location = new System.Drawing.Point(229, 9);
+            this.label1.Font = new System.Drawing.Font("Microsoft Himalaya", 60.25F);
+            this.label1.Location = new System.Drawing.Point(31, 84);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(300, 34);
+            this.label1.Size = new System.Drawing.Size(711, 81);
             this.label1.TabIndex = 0;
             this.label1.Text = "Асинхронный RS триггер";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(217, 75);
+            this.pictureBox1.Image = global::videoprogram.Properties.Resources.gif2;
+            this.pictureBox1.Location = new System.Drawing.Point(235, 75);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(312, 176);
+            this.pictureBox1.Size = new System.Drawing.Size(395, 176);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // tick1
+            // 
+            this.tick1.AutoSize = true;
+            this.tick1.Location = new System.Drawing.Point(194, 84);
+            this.tick1.Name = "tick1";
+            this.tick1.Size = new System.Drawing.Size(0, 13);
+            this.tick1.TabIndex = 2;
+            // 
+            // tick2
+            // 
+            this.tick2.AutoSize = true;
+            this.tick2.Location = new System.Drawing.Point(194, 228);
+            this.tick2.Name = "tick2";
+            this.tick2.Size = new System.Drawing.Size(0, 13);
+            this.tick2.TabIndex = 3;
+            // 
+            // tick3
+            // 
+            this.tick3.AutoSize = true;
+            this.tick3.Location = new System.Drawing.Point(496, 106);
+            this.tick3.Name = "tick3";
+            this.tick3.Size = new System.Drawing.Size(0, 13);
+            this.tick3.TabIndex = 4;
+            // 
+            // tick4
+            // 
+            this.tick4.AutoSize = true;
+            this.tick4.Location = new System.Drawing.Point(496, 216);
+            this.tick4.Name = "tick4";
+            this.tick4.Size = new System.Drawing.Size(0, 13);
+            this.tick4.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(769, 281);
+            this.Controls.Add(this.tick4);
+            this.Controls.Add(this.tick3);
+            this.Controls.Add(this.tick2);
+            this.Controls.Add(this.tick1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "-_-";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -72,6 +115,10 @@ namespace videoprogram
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label tick1;
+        private System.Windows.Forms.Label tick2;
+        private System.Windows.Forms.Label tick3;
+        private System.Windows.Forms.Label tick4;
     }
 }
 
--- videoprogram/videoprogram/Form1.cs
+++ videoprogram/videoprogram/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace videoprogram
 {
@@ -15,11 +16,34 @@ namespace videoprogram
         public Form1()
         {
             InitializeComponent();
-            pictureBox1.Image = Image.FromFile("");
+            pictureBox1.Image = Image.FromFile("C:/Users/NANI/Desktop/videoprogram/video/videoprogram/videostuff/gif2.gif");
+            pictureBox1.Hide();
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
+            for (int font = 60; font >= 25; font--)
+            {
+                label1.Font = new Font("Microsoft Himalaya", font);
+                Thread.Sleep(10);
+            }
+            label1.Location = new Point(10, 10);
+            Thread.Sleep(500);
+            pictureBox1.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Thread ttiking = new Thread(new ThreadStart(tiking));
+            
+        }
+        public static void tiking()
+        {
+            Form1.
 
         }
     }
