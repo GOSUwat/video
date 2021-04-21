8afefd2776a2cd55ddd1819ed6d015c9eb555395
GOSUwat <asus6972@gmail.com>
2021-04-21 21:56:32 +0300

    graphics
    13

--- videoprogram/videoprogram/Form1.Designer.cs
+++ videoprogram/videoprogram/Form1.Designer.cs
@@ -35,6 +35,12 @@ namespace videoprogram
             this.tick2 = new System.Windows.Forms.Label();
             this.tick3 = new System.Windows.Forms.Label();
             this.tick4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,10 +57,9 @@ namespace videoprogram
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::videoprogram.Properties.Resources.gif2;
-            this.pictureBox1.Location = new System.Drawing.Point(235, 75);
+            this.pictureBox1.Location = new System.Drawing.Point(171, 75);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(395, 176);
+            this.pictureBox1.Size = new System.Drawing.Size(586, 210);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -91,11 +96,77 @@ namespace videoprogram
             this.tick4.Size = new System.Drawing.Size(0, 13);
             this.tick4.TabIndex = 5;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label2.Location = new System.Drawing.Point(422, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 31);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "R";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label3.Location = new System.Drawing.Point(422, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 31);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "S";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label4.Location = new System.Drawing.Point(483, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 31);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label5.Location = new System.Drawing.Point(483, 210);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 31);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label6.Location = new System.Drawing.Point(593, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 31);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Q";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label7.Location = new System.Drawing.Point(593, 175);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 31);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "!Q";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(769, 281);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.tick4);
             this.Controls.Add(this.tick3);
             this.Controls.Add(this.tick2);
@@ -119,6 +190,12 @@ namespace videoprogram
         private System.Windows.Forms.Label tick2;
         private System.Windows.Forms.Label tick3;
         private System.Windows.Forms.Label tick4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- videoprogram/videoprogram/Form1.cs
+++ videoprogram/videoprogram/Form1.cs
@@ -16,35 +16,67 @@ namespace videoprogram
         public Form1()
         {
             InitializeComponent();
-            pictureBox1.Image = Image.FromFile("C:/Users/NANI/Desktop/videoprogram/video/videoprogram/videostuff/gif2.gif");
-            pictureBox1.Hide();
+            //pictureBox1.Hide();
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
-            for (int font = 60; font >= 25; font--)
+            /*for (int font = 60; font >= 25; font--)
             {
                 label1.Font = new Font("Microsoft Himalaya", font);
                 Thread.Sleep(10);
             }
             label1.Location = new Point(10, 10);
-            Thread.Sleep(500);
+            Thread.Sleep(500);*/
             pictureBox1.Show();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //pictureBox1.BackColor = Color.White;
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            Thread ttiking = new Thread(new ThreadStart(tiking));
-            
-        }
-        public static void tiking()
-        {
-            Form1.
+            draw();
 
+
+
+
+        }
+        private void draw()
+        {
+            int x = 3;
+            Graphics g;
+            Bitmap bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bitmap);
+            Pen penblck = new Pen(Color.Black,x);
+            Pen penred = new Pen(Color.Red,x);
+            Pen penblue = new Pen(Color.Blue,x);
+            
+            g.DrawRectangle(penblck, 300, 10, 50, 60); //Square R
+            g.DrawRectangle(penblck, 300, 120, 50, 60); //Square S
+
+            //Vhod R
+            g.DrawLine(penred,300,30,250,30);
+            //Vhod S
+            g.DrawLine(penred, 300, 140, 250, 140);
+            //Vihod R
+            g.DrawLine(penred, 351, 30, 450, 30);
+            //Vihod S
+            g.DrawLine(penred, 351, 140, 450, 140);
+            //R to S
+            g.DrawLine(penblue, 400, 30, 400, 90);
+            g.DrawLine(penblue, 402, 90, 270, 90);
+            g.DrawLine(penblue, 270, 90, 270, 130);
+            g.DrawLine(penblue, 269, 130, 300, 130);
+            //S to R
+            g.DrawLine(penred, 380, 80, 380, 140);
+            g.DrawLine(penred, 382, 80, 270, 80);
+            g.DrawLine(penred, 270, 40, 270, 82);
+            g.DrawLine(penred, 270, 40, 300, 40);
+
+            pictureBox1.Image = bitmap;
         }
     }
 }
